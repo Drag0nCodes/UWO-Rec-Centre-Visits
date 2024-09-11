bb07fa47946328aed97ac42caee298450bed0153
Colin Brown <cbrow388@uwo.ca>
2024-09-11 15:48:28 -0400

    - Code will now add new tweets to the end of the Outout.csv file - Excel file will read the data directly from the Output file - The code will ask to stop adding new tweet data when it thinks the tweet it got has caught up to the data already in the Output file - No manual copying data anymore - Fixed issues with the eastern time conversion when near midnight not setting the day properly - Removed the code saving the next curser

--- RecCentreVisits-pyApp/main.py
+++ RecCentreVisits-pyApp/main.py
@@ -6,10 +6,6 @@ from tweety.exceptions import ActionRequired, RateLimitReached
 from tweety import Twitter
 import re;
 EASTERNTIMEOFFSET = -4
-
-f = open("output.csv", 'w')  # Open and clear output.csv
-tweetsCSVText = ""  # Will hold each pages scraped information
-
 app = Twitter("session")  # Create a twitter session
 
 # Sign in to new session
@@ -24,28 +20,80 @@ app.connect()
 
 print("Requesting page 1")
 all_tweets = app.get_tweets("WesternWeightRm")  # Get first twitter page
-# all_tweets = app.get_tweets("WesternWeightRm", 1, False, 2, 'DAABCgABGP-hVAG__GMKAAIYymPTvtdg4AgAAwAAAAIAAA') # Get specified twitter page with cursor
 lastTweets = ""  # Hold the previous all_tweets value
 tweetsCount = 0;
+stop = False
+first = True
 
-for times in range(0, 100):  # Get x number of twitter pages
-    tweetsCount += len(all_tweets.tweets)
+f = open("Output.csv", 'a+')  # Open for read write output.csv
+f.seek(0, 2)
+if f.tell() != 0:
+    f.seek(f.tell() - 1)  # Move pointer one character back
+    lastDate = ""
+    while f.tell() > 0:  # Get last line
+        f.seek(f.tell() - 2)  # Move pointer one character back
+        char = f.read(1)
+        if char == "\n":  # Newline character found
+            break
+
+# Read the last line
+lastDate = f.readline()
+
+f.seek(0, 2)
+if lastDate is not None:  # Get info of last tweet in CSV file
+    lastMonth = int(re.findall("-\d+-", lastDate)[0][1:-1])
+    lastDay = int(re.findall("-\d+", lastDate)[1][1:])
+    lastHour = int(re.findall("\d+:", lastDate)[0][:-1])
+    lastWR = int(re.findall(",\d+", lastDate)[0][1:])
+tweetsCSVText = ""  # Will hold each pages scraped information
+tweetsCount = 0
+
+for times in range(0, 4):  # Get x number of twitter pages
     for tweet in all_tweets:  # Process each tweet on page (usually 20 of them)
         tweetTxt = tweet.text.lower()  # The text of the tweet
+
         if "wr" not in tweetTxt:  # Tweet does not have "wr"
             print("Could not find \'WR\' - Skipping tweet on  " + str(tweet.date) + ": " + tweetTxt + "\n\n")
             break
 
-        matches = re.findall("\w*\d+", tweetTxt)  # Get all numbers
+        matches = re.findall("\d+", tweetTxt)  # Get all numbers
         num = matches[0]  # WR num should be the first number
 
         tweetDate = str(tweet.date)
-        dateHour = int(re.findall(" \d+", tweetDate)[0][1:3])  # Get hour of tweet
-        tweetDateNew = re.findall("\S+ ", tweetDate)[0] + str((dateHour + EASTERNTIMEOFFSET)%24) + re.findall(":\d+:\d+", tweetDate)[0]  # New format of date
+        tweetMonth = int(re.findall("-\d+-", tweetDate)[0][1:-1])
+        tweetDay = int(re.findall("-\d+", tweetDate)[1][1:])
+        tweetHour = int(re.findall(" \d+", tweetDate)[0][1:3])  # Get hour of tweet
 
-        tweetsCSVText += tweetDateNew + ", " + num + "\n"  # Save the tweet data to string
-    f.write(tweetsCSVText)  # Write the last page to csv file
-    tweetsCSVText = ""  # Reset string for next page
+        if tweetHour < -EASTERNTIMEOFFSET:  # Account for stupid ETC to ET time conversions
+            tweetDay -= 1
+            if tweetDay < 0:
+                tweetMonth -= 1
+                tweetDay = 30
+                if tweetMonth == 0:
+                    tweetMonth = 12
+
+        tweetHour = (tweetHour + EASTERNTIMEOFFSET) % 24  # Change tweets hour to eastern time
+
+        if tweetMonth == lastMonth and tweetDay == lastDay and tweetHour == lastHour and int(num) == lastWR:  # Stop code when tweet is caught up
+            print("Matching tweet with start of CSV file. \nNew tweet time:" + re.findall(" \d+:\d+", tweetDate)[0] + "/" + str(tweetHour) + re.findall(":\d+", tweetDate)[0] + "\nStart of CSV time:" + re.findall(" .+,", lastDate)[0][:-1] + "\nEnter 'S' to stop.")
+            input1 = input().lower()
+            if input1 == 's':
+                stop = True
+                break
+
+        if tweetMonth <= lastMonth and tweetDay < lastDay:
+            print("Got too many tweets, cleanup CSV file around " + str(lastMonth) + "-" + str(lastDay))
+            stop = True
+            break
+
+        tweetDateNew = re.findall("\d+", tweetDate)[0] + "-" + str(tweetMonth).zfill(2) + "-" + str(tweetDay).zfill(2) + " " + str(tweetHour) + re.findall(":\d+:", tweetDate)[0][:-1]  # New format of date
+        tweetsCSVText = tweetDateNew + "," + num + "\n" + tweetsCSVText  # Save the tweet data to start of string
+        tweetsCount += 1
+
+    if stop:
+        print("Stopping")
+        break
+
     if not all_tweets.is_next_page:
         print("No pages left")
 
@@ -55,14 +103,11 @@ for times in range(0, 100):  # Get x number of twitter pages
                 all_tweets.tweets[(len(all_tweets.tweets) - 1)].date) + " with cursor: " + all_tweets.cursor)
         else:
             print("Page " + str(times + 1) + " had no tweets, requesting page " + str(times + 2) + " with cursor: " + all_tweets.cursor)
-        lastTweets = all_tweets  # Save tweet page
         all_tweets = app.get_tweets("WesternWeightRm", 1, False, 2, all_tweets.cursor)  # Get next page of tweets
     except RateLimitReached as e:  # RateLimit reached error with twitter (BRING BACK FREE API USAGE)
-        print("\nOn twitter request " + str(times + 2) + ": " + e.message + "Was able to collect " + str(
-            tweetsCount) + " tweets\nThe last cursor for next tweets (also written to next_cursor.txt, may reread up to 20 repeat tweets):" + lastTweets.cursor)
-        g = open("next_cursor.txt", 'w')
-        g.write("Last cursor: " + lastTweets.cursor + "\ncursor_top: " + lastTweets.cursor_top)  # Save cursor info to file
-        g.close()
+        print("\nOn twitter request " + str(times + 2) + ": " + e.message)
         break
 
+f.write(tweetsCSVText)
+print("Added " + str(tweetsCount) + " new data points to Output.csv")
 f.close()
